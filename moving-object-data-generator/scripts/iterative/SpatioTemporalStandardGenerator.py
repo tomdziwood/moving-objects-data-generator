e844bc35edb3f61d4bed3c1c783a3a15297fbed5
tomdziwood <tomdziwood@gmail.com>
2022-06-23 10:11:25 +0200

    Reformat current code
    - add comments to the main scripts
    - change file writing format: semicolon as a separator

--- moving-object-data-generator/scripts/iterative/SpatioTemporalStandardGenerator.py
+++ moving-object-data-generator/scripts/iterative/SpatioTemporalStandardGenerator.py
@@ -4,29 +4,36 @@ import numpy as np
 def generate(output_file="SpatioTemporalStandardGenerator_output_file.txt", time_frames_number=10, area=1000, cell_size=5, n_colloc=3, lambda_1=5, lambda_2=100, m_clumpy=1, m_overlap=1, ncfr=1.0, ncfn=1.0, ndf=2, ndfn=5000, random_seed=None):
     print("generate()")
 
+    # set random seed value
     if random_seed is not None:
         np.random.seed(random_seed)
 
+    # open file to which output will be written
     f = open(file=output_file, mode="w")
 
+    # determine length to each of the n_colloc basic co-locations with poisson distribution (lam=lambda_1)
     base_collocation_lengths = np.random.poisson(lam=lambda_1, size=n_colloc)
     base_collocation_lengths[base_collocation_lengths < 2] = 2
     print("base_collocation_lengths=%s" % str(base_collocation_lengths))
 
+    # set final length to each of the co-locations according to the m_overlap parameter value
     if m_overlap > 1:
         collocation_lengths = np.repeat(a=base_collocation_lengths + 1, repeats=m_overlap)
     else:
         collocation_lengths = base_collocation_lengths
     print("collocation_lengths=%s" % str(collocation_lengths))
 
+    # determine number of instances to each of the co-locations with poisson distribution (lam=lambda_2)
     collocation_instances_counts = np.random.poisson(lam=lambda_2, size=n_colloc * m_overlap)
     print("collocation_instances_counts=%s" % str(collocation_instances_counts))
 
+    # determine the total number of features, which take part in co-locations
     collocation_features_sum = np.sum(base_collocation_lengths)
     if m_overlap > 1:
         collocation_features_sum += n_colloc * m_overlap
     print("collocation_features_sum=%d" % collocation_features_sum)
 
+    # count all instances of every i'th co-location feature
     collocation_features_instances_counts = np.zeros(shape=collocation_features_sum, dtype=np.int32)
     collocation_start_feature_id = 0
     for i_colloc in range(n_colloc * m_overlap):
@@ -37,22 +44,25 @@ def generate(output_file="SpatioTemporalStandardGenerator_output_file.txt", time
             collocation_start_feature_id += collocation_lengths[i_colloc] + m_overlap - 1
     print("collocation_features_instances_counts=%s" % str(collocation_features_instances_counts))
 
+    # express area dimension in spatial cell unit
     area_in_cell_dim = area // cell_size
     print("area_in_cell_dim: ", area_in_cell_dim)
 
-    # collocation noise features
+    # determine the total number of features, which take part in co-locations and also are used to generate noise
     collocation_noise_features_sum = round(ncfr * collocation_features_sum)
     print("collocation_noise_features_sum=%d" % collocation_noise_features_sum)
 
+    # chose co-location noise features from co-location features
     collocation_noise_features = np.random.choice(a=collocation_features_sum, size=collocation_noise_features_sum, replace=False)
     collocation_noise_features.sort()
     print("collocation_noise_features=%s" % str(collocation_noise_features))
 
+    # prepare array which holds counts of created instances of the co-location noise feature
     collocation_noise_features_instances_counts = ncfn * collocation_features_instances_counts[collocation_noise_features]
     collocation_noise_features_instances_counts = collocation_noise_features_instances_counts.astype(np.int32)
     print("collocation_noise_features_instances_counts=%s" % str(collocation_noise_features_instances_counts))
 
-    # additional noise feature
+    # determine number of each of the additional noise features if they are requested
     (additional_noise_features, additional_noise_features_instances_counts) = (np.array([]), np.array([]))
     if ndf > 0:
         additional_noise_features_ids = np.random.randint(low=ndf, size=ndfn) + collocation_features_sum
@@ -60,53 +70,71 @@ def generate(output_file="SpatioTemporalStandardGenerator_output_file.txt", time
         print("additional_noise_features=%s" % str(additional_noise_features))
         print("additional_noise_features_instances_counts=%s" % str(additional_noise_features_instances_counts))
 
+    # generate data for each time frame
     for i_time_frame in range(time_frames_number):
         print("i_time_frame=%d" % i_time_frame)
 
         collocation_start_feature_id = 0
         collocation_features_instances_counters = np.zeros(shape=collocation_features_sum, dtype=np.int32)
 
+        # generate data of every co-location in given time frame
         for i_colloc in range(n_colloc * m_overlap):
+
+            # get the features ids of current co-location
             collocation_features = np.arange(collocation_start_feature_id, collocation_start_feature_id + collocation_lengths[i_colloc])
             collocation_features[-1] += i_colloc % m_overlap
             print("collocation_features=%s" % str(collocation_features))
 
+            # generate data of every instance of current co-location
             collocation_instance_id = 0
             while collocation_instance_id < collocation_instances_counts[i_colloc]:
+
+                # determine spatial cell with uniform distribution
                 cell_x_id = np.random.randint(low=area_in_cell_dim)
                 cell_y_id = np.random.randint(low=area_in_cell_dim)
                 # print("ids:\t(%d, %d)" % (cell_x_id, cell_y_id))
 
+                # get starting coordinations of chosen cell
                 cell_x = cell_x_id * cell_size
                 cell_y = cell_y_id * cell_size
                 # print("cell coor:\t(%d, %d)" % (cell_x, cell_y))
 
+                # determine number of instances placed in the same spatial cell according to the m_clumpy parameter value
                 m_clumpy_repeats = min(m_clumpy, collocation_instances_counts[i_colloc] - collocation_instance_id)
+
+                # place m_clumpy_repeats number of co-location instances in the chosen cell
                 for _ in range(m_clumpy_repeats):
+
+                    # determine coordinations of every co-location feature
                     for collocation_feature in collocation_features:
                         instance_x = cell_x + np.random.uniform() * cell_size
                         instance_y = cell_y + np.random.uniform() * cell_size
                         # print("collocation_feature: %d\tinst coor:\t(%f, %f)" % (collocation_feature, instance_x, instance_y))
-                        f.write("%d %d %d %f %f\n" % (i_time_frame, collocation_feature, collocation_features_instances_counters[collocation_feature], instance_x, instance_y))
+                        f.write("%d;%d;%d;%.6f;%.6f\n" % (i_time_frame, collocation_feature, collocation_features_instances_counters[collocation_feature], instance_x, instance_y))
                         collocation_features_instances_counters[collocation_feature] += 1
 
                     collocation_instance_id += 1
 
+            # change starting feature of next co-location according to the m_overlap parameter value
             if (i_colloc + 1) % m_overlap == 0:
                 collocation_start_feature_id += collocation_lengths[i_colloc] + m_overlap - 1
 
+        # generate data of every co-location noise feature in given time frame
         for i_feature in range(collocation_noise_features_sum):
             collocation_noise_feature_id = collocation_noise_features[i_feature]
             collocation_noise_feature_instances_count = collocation_noise_features_instances_counts[i_feature]
             # print("collocation_noise_feature_instances_count=%d" % collocation_noise_feature_instances_count)
             collocation_noise_feature_instance_id = collocation_features_instances_counts[collocation_noise_feature_id]
+
+            # generate data of every instance of current co-location noise feature
             for _ in range(collocation_noise_feature_instances_count):
                 instance_x = np.random.uniform(high=area)
                 instance_y = np.random.uniform(high=area)
                 # print("noise feature %d \t feature instance %d \t coor:\t(%f, %f)" % (collocation_noise_feature_id, collocation_noise_feature_instance_id, instance_x, instance_y))
-                f.write("%d %d %d %f %f\n" % (i_time_frame, collocation_noise_feature_id, collocation_noise_feature_instance_id, instance_x, instance_y))
+                f.write("%d;%d;%d;%.6f;%.6f\n" % (i_time_frame, collocation_noise_feature_id, collocation_noise_feature_instance_id, instance_x, instance_y))
                 collocation_noise_feature_instance_id += 1
 
+        # generate additional noise features if they are requested in given time frame
         if ndf > 0:
             for i_feature in range(additional_noise_features.size):
                 additional_noise_feature = additional_noise_features[i_feature]
@@ -114,8 +142,9 @@ def generate(output_file="SpatioTemporalStandardGenerator_output_file.txt", time
                     instance_x = np.random.uniform(high=area)
                     instance_y = np.random.uniform(high=area)
                     # print("additional noise %d inst %d \t coor:\t(%f, %f)" % (collocation_start_feature_id + additional_noise_feature, additional_noise_feature_instance[additional_noise_feature], instance_x, instance_y))
-                    f.write("%d %d %d %f %f\n" % (i_time_frame, additional_noise_feature, additional_noise_feature_instance_id, instance_x, instance_y))
+                    f.write("%d;%d;%d;%.6f;%.6f\n" % (i_time_frame, additional_noise_feature, additional_noise_feature_instance_id, instance_x, instance_y))
 
+    # end of file writing
     f.close()
 
 
--- moving-object-data-generator/scripts/vectorized/SpatioTemporalStandardGenerator.py
+++ moving-object-data-generator/scripts/vectorized/SpatioTemporalStandardGenerator.py
@@ -4,29 +4,36 @@ import numpy as np
 def generate(output_file="SpatialStandardGenerator_output_file.txt", time_frames_number=10, area=1000, cell_size=5, n_colloc=3, lambda_1=5, lambda_2=100, m_clumpy=1, m_overlap=1, ncfr=1.0, ncfn=1.0, ndf=2, ndfn=5000, random_seed=None):
     print("generate()")
 
+    # set random seed value
     if random_seed is not None:
         np.random.seed(random_seed)
 
+    # open file to which output will be written
     f = open(file=output_file, mode="w")
 
+    # determine length to each of the n_colloc basic co-locations with poisson distribution (lam=lambda_1)
     base_collocation_lengths = np.random.poisson(lam=lambda_1, size=n_colloc)
     base_collocation_lengths[base_collocation_lengths < 2] = 2
     print("base_collocation_lengths=%s" % str(base_collocation_lengths))
 
+    # set final length to each of the co-locations according to the m_overlap parameter value
     if m_overlap > 1:
         collocation_lengths = np.repeat(a=base_collocation_lengths + 1, repeats=m_overlap)
     else:
         collocation_lengths = base_collocation_lengths
     print("collocation_lengths=%s" % str(collocation_lengths))
 
+    # determine number of instances to each of the co-locations with poisson distribution (lam=lambda_2)
     collocations_instances_counts = np.random.poisson(lam=lambda_2, size=n_colloc * m_overlap)
     print("collocations_instances_counts=%s" % str(collocations_instances_counts))
 
+    # determine the total number of features, which take part in co-locations
     collocation_features_sum = np.sum(base_collocation_lengths)
     if m_overlap > 1:
         collocation_features_sum += n_colloc * m_overlap
     print("collocation_features_sum=%d" % collocation_features_sum)
 
+    # count all instances of every i'th co-location feature
     collocation_features_instances_counts = np.zeros(shape=collocation_features_sum, dtype=np.int32)
     collocation_start_feature_id = 0
     for i_colloc in range(n_colloc * m_overlap):
@@ -37,42 +44,50 @@ def generate(output_file="SpatialStandardGenerator_output_file.txt", time_frames
             collocation_start_feature_id += collocation_lengths[i_colloc] + m_overlap - 1
     print("collocation_features_instances_counts=%s" % str(collocation_features_instances_counts))
 
-    collocation_start_feature_id = 0
+    # express area dimension in spatial cell unit
     area_in_cell_dim = area // cell_size
     print("area_in_cell_dim: ", area_in_cell_dim)
 
-    # collocation noise feature
+    # determine the total number of features, which take part in co-locations and also are used to generate noise
     collocation_noise_features_sum = round(ncfr * collocation_features_sum)
     print("collocation_noise_features_sum=%d" % collocation_noise_features_sum)
 
+    # chose co-location noise features from co-location features
     collocation_noise_features = np.random.choice(a=collocation_features_sum, size=collocation_noise_features_sum, replace=False)
     collocation_noise_features.sort()
     print("collocation_noise_features=%s" % str(collocation_noise_features))
 
+    # prepare array which holds counts of created instances of the co-location noise feature
     collocation_noise_features_instances_counts = ncfn * collocation_features_instances_counts[collocation_noise_features]
     collocation_noise_features_instances_counts = collocation_noise_features_instances_counts.astype(np.int32)
     print("collocation_noise_features_instances_counts=%s" % str(collocation_noise_features_instances_counts))
 
-    # additional noise feature
+    # determine number of each of the additional noise features if they are requested
     if ndf > 0:
         additional_noise_features_ids = np.random.randint(low=ndf, size=ndfn) + collocation_features_sum
         (additional_noise_features, additional_noise_features_instances_counts) = np.unique(ar=additional_noise_features_ids, return_counts=True)
         print("additional_noise_features=%s" % str(additional_noise_features))
         print("additional_noise_features_instances_counts=%s" % str(additional_noise_features_instances_counts))
 
+    # generate data for each time frame
     for i_time_frame in range(time_frames_number):
         print("i_time_frame=%d" % i_time_frame)
 
         collocation_start_feature_id = 0
         collocation_features_instances_counters = np.zeros(shape=collocation_features_sum, dtype=np.int32)
 
+        # generate data of every co-location in given time frame
         for i_colloc in range(n_colloc * m_overlap):
+
+            # get the features ids of current co-location
             collocation_features = np.arange(collocation_start_feature_id, collocation_start_feature_id + collocation_lengths[i_colloc])
             collocation_features[-1] += i_colloc % m_overlap
             print("collocation_features=%s" % str(collocation_features))
 
+            # calculate total number of all co-location feature instances
             collocation_features_instances_sum = collocations_instances_counts[i_colloc] * collocation_lengths[i_colloc]
 
+            # generate vector of x coordinates of all the consecutive instances
             collocation_features_instances_x = np.random.randint(low=area_in_cell_dim, size=(collocations_instances_counts[i_colloc] - 1) // m_clumpy + 1)
             collocation_features_instances_x *= cell_size
             collocation_features_instances_x = collocation_features_instances_x.astype(dtype=np.float64)
@@ -80,6 +95,7 @@ def generate(output_file="SpatialStandardGenerator_output_file.txt", time_frames
             collocation_features_instances_x = np.repeat(a=collocation_features_instances_x, repeats=collocation_lengths[i_colloc])
             collocation_features_instances_x += np.random.uniform(high=cell_size, size=collocation_features_instances_sum)
 
+            # generate vector of y coordinates of all the consecutive instances
             collocation_features_instances_y = np.random.randint(low=area_in_cell_dim, size=(collocations_instances_counts[i_colloc] - 1) // m_clumpy + 1)
             collocation_features_instances_y *= cell_size
             collocation_features_instances_y = collocation_features_instances_y.astype(dtype=np.float64)
@@ -87,8 +103,10 @@ def generate(output_file="SpatialStandardGenerator_output_file.txt", time_frames
             collocation_features_instances_y = np.repeat(a=collocation_features_instances_y, repeats=collocation_lengths[i_colloc])
             collocation_features_instances_y += np.random.uniform(high=cell_size, size=collocation_features_instances_sum)
 
+            # generate vector of features ids of all the consecutive instances
             collocation_features_ids = np.tile(A=collocation_features, reps=collocations_instances_counts[i_colloc])
 
+            # generate vector of features instances ids of all the consecutive instances
             collocation_features_instances_ids = np.arange(
                 start=collocation_features_instances_counters[collocation_start_feature_id],
                 stop=collocation_features_instances_counters[collocation_start_feature_id] + collocations_instances_counts[i_colloc]
@@ -100,36 +118,45 @@ def generate(output_file="SpatialStandardGenerator_output_file.txt", time_frames
             ))
             collocation_features_instances_ids = collocation_features_instances_ids.T.flatten()
 
-            time_frame_ids = np.ones(shape=collocation_features_instances_sum, dtype=np.int32) * i_time_frame
+            # generate vector of time frame ids of current time frame
+            time_frame_ids = np.full(shape=collocation_features_instances_sum, fill_value=i_time_frame, dtype=np.int32)
 
-            fmt = '%d %d %d %.6f %.6f\n' * collocation_features_instances_sum
+            # write data of all the co-location features to the output file
+            fmt = '%d;%d;%d;%.6f;%.6f\n' * collocation_features_instances_sum
             data = fmt % tuple(np.column_stack(tup=(time_frame_ids, collocation_features_ids, collocation_features_instances_ids, collocation_features_instances_x, collocation_features_instances_y)).ravel())
             f.write(data)
 
+            # increase counts of created instances of the co-location features which occurred in current co-location
             collocation_features_instances_counters[collocation_features] += collocations_instances_counts[i_colloc]
 
+            # change starting feature of next co-location according to the m_overlap parameter value
             if (i_colloc + 1) % m_overlap == 0:
                 collocation_start_feature_id += collocation_lengths[i_colloc] + m_overlap - 1
 
-        # collocation noise feature
+        # generate data of every co-location noise feature in given time frame
+        # generate vectors of x and y coordinates and ids of all the consecutive instances of co-location noise features
         collocation_noise_features_instances_sum = np.sum(collocation_noise_features_instances_counts)
         collocation_noise_features_instances_x = np.random.uniform(high=area, size=collocation_noise_features_instances_sum)
         collocation_noise_features_instances_y = np.random.uniform(high=area, size=collocation_noise_features_instances_sum)
         collocation_noise_features_ids = np.repeat(a=collocation_noise_features, repeats=collocation_noise_features_instances_counts)
 
+        # generate vector of features instances ids of all the consecutive instances of co-location noise features
         start = collocation_features_instances_counts[collocation_noise_features]
         length = collocation_noise_features_instances_counts
         collocation_noise_features_instances_ids = np.repeat(a=(start + length - length.cumsum()), repeats=length) + np.arange(collocation_noise_features_instances_sum)
         # print("collocation_noise_features_instances_ids=%s" % collocation_noise_features_instances_ids)
 
-        time_frame_ids = np.ones(shape=collocation_noise_features_instances_sum, dtype=np.int32) * i_time_frame
+        # generate vector of time frame ids of current time frame
+        time_frame_ids = np.full(shape=collocation_noise_features_instances_sum, fill_value=i_time_frame, dtype=np.int32)
 
-        fmt = '%d %d %d %.6f %.6f\n' * collocation_noise_features_instances_sum
+        # write data of all the co-location noise features to the output file
+        fmt = '%d;%d;%d;%.6f;%.6f\n' * collocation_noise_features_instances_sum
         data = fmt % tuple(np.column_stack(tup=(time_frame_ids, collocation_noise_features_ids, collocation_noise_features_instances_ids, collocation_noise_features_instances_x, collocation_noise_features_instances_y)).ravel())
         f.write(data)
 
-        # additional noise feature
+        # generate additional noise features if they are requested in given time frame
         if ndf > 0:
+            # generate vectors of all necessary additional noise features data
             additional_noise_features_instances_ids = np.repeat(
                 a=(additional_noise_features_instances_counts - additional_noise_features_instances_counts.cumsum()),
                 repeats=additional_noise_features_instances_counts
@@ -138,12 +165,14 @@ def generate(output_file="SpatialStandardGenerator_output_file.txt", time_frames
             additional_noise_features_instances_x = np.random.uniform(high=area, size=ndfn)
             additional_noise_features_instances_y = np.random.uniform(high=area, size=ndfn)
 
-            time_frame_ids = np.ones(shape=ndfn, dtype=np.int32) * i_time_frame
+            time_frame_ids = np.full(shape=ndfn, fill_value=i_time_frame, dtype=np.int32)
 
-            fmt = '%d %d %d %.6f %.6f\n' * ndfn
+            # write data of all the additional noise features to the output file
+            fmt = '%d;%d;%d;%.6f;%.6f\n' * ndfn
             data = fmt % tuple(np.column_stack(tup=(time_frame_ids, additional_noise_features_ids, additional_noise_features_instances_ids, additional_noise_features_instances_x, additional_noise_features_instances_y)).ravel())
             f.write(data)
 
+    # end of file writing
     f.close()
 
 
